d43f927e2430e28eb50bf0365dd8b31c6095f0b3
Piyush Gupta <piyushgupta.gm@gmail.com>
2020-09-02 19:14:12 +0530

    Starter App

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,9 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+    <h1>Welcome to blank app</h1>
   </q-page>
 </template>
 
 <script>
-export default {
-  name: 'PageIndex'
-}
+export default {}
 </script>
